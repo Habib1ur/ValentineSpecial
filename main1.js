d4600df3bd83a90d86673ec503b59b60ac7f2947
Habib1ur <habiburrahman.1.chowdhury@gmail.com>
2026-02-09 11:16:39 +0600

    video added

--- main1.js
+++ main1.js
@@ -1,7 +1,39 @@
 onload = () => {
-    const c = setTimeout(() => {
-      document.body.classList.remove("not-loaded");
-      clearTimeout(c);
-    }, 1000);
+  const c = setTimeout(() => {
+    document.body.classList.remove("not-loaded");
+    clearTimeout(c);
+  }, 1000);
+
+  const video = document.querySelector(".night__video");
+
+  if (!video) {
+    return;
+  }
+
+  const tryPlayWithSound = async () => {
+    video.muted = false;
+    try {
+      await video.play();
+      return true;
+    } catch (error) {
+      video.muted = true;
+      try {
+        await video.play();
+      } catch (playError) {
+        return false;
+      }
+    }
+    return false;
   };
-  
+
+  tryPlayWithSound();
+
+  const resumeWithSound = () => {
+    tryPlayWithSound();
+    document.removeEventListener("pointerdown", resumeWithSound);
+    document.removeEventListener("keydown", resumeWithSound);
+  };
+
+  document.addEventListener("pointerdown", resumeWithSound);
+  document.addEventListener("keydown", resumeWithSound);
+};
